c1c6b9c8395f4603e23bb534260a8809997c8921
hemit patel 🧑🏽‍💻 <hemitvpatel@gmail.com>
2025-10-16 16:25:50 -0400

    Implement LED display patterns for Smiley and letters
    Added functions to display patterns on LED matrix.

--- main.ino
+++ main.ino
@@ -1,3 +1,12 @@
+#include <LedControl.h>
+
+const int DIN = 11;
+const int CLK = 13;
+const int CS = 3;
+const int UNITS = 1;
+
+LedControl lc(DIN,CLK,CS,UNITS);
+
 const int redLight1 = 10;
 const int yellowLight1 = 9;
 const int greenLight1 = 8;
@@ -16,6 +25,9 @@ void setup() {
   pinMode(redLight2, OUTPUT);
   pinMode(yellowLight2, OUTPUT);
   pinMode(greenLight2, OUTPUT);
+  lc.shutdown(0, false);
+  lc.setIntensity(0,8);
+  lc.clearDisplay(0);
 }
 
 void runRedLights() {
@@ -45,17 +57,47 @@ void runGreenLights() {
     digitalWrite(redLight2, HIGH);
 }
 
+void runSmileyFace() {
+  lc.clearDisplay(0);
+  lc.setColumn(0, 2, B01100110);
+  lc.setColumn(0, 5, B01000010);
+  lc.setColumn(0,6,B01111110);
+}
+
+void runHLetter() {
+  lc.clearDisplay(0);
+  lc.setRow(0, 1, B11111111);
+  lc.setRow(0, 2, B11111111);
+
+  lc.setRow(0, 5, B11111111);
+  lc.setRow(0, 6, B11111111);
+
+  lc.setColumn(0, 3, B01111110);
+  lc.setColumn(0, 4, B01111110);
+  lc.setColumn(0, 5, B01111110);
+}
+
+void runILetter() {
+  lc.clearDisplay(0);
+  lc.setColumn(0, 2, B00011000);
+  lc.setRow(0, 3, B00110111);
+  lc.setRow(0, 4, B00110111);
+}
+
 void loop() {
   if (counter == 0) {
     runRedLights();
+    runSmileyFace();
   }
 
   else if (counter == 1) {
     runYellowLights();
+    runHLetter();
   }
 
   else if (counter == 2){
     runGreenLights();
+    runILetter();
   }
 
   counter++;
