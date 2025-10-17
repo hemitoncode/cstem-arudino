efd50c7b6410b533a9a050ca52c9e0e4bcb1617e
hemit patel 🧑🏽‍💻 <hemitvpatel@gmail.com>
2025-10-16 20:36:01 -0400

    Refactor light control variables and functions

--- main.ino
+++ main.ino
@@ -5,73 +5,77 @@ const int CLK = 13;
 const int CS = 3;
 const int UNITS = 1;
 
-LedControl lc(DIN,CLK,CS,UNITS);
+LedControl lc(DIN, CLK, CS, UNITS);
 
-const int redLight1 = 10;
-const int yellowLight1 = 9;
-const int greenLight1 = 8;
+// A lights
+const int redLightA = 10;
+const int yellowLightA = 9;
+const int greenLightA = 8;
 
-const int redLight2 = 2;
-const int yellowLight2 = 4;
-const int greenLight2 = 12;
+// B lights
+const int redLightB = 2;
+const int yellowLightB = 4;
+const int greenLightB = 12;
 
 unsigned int counter = 0;
 
 void setup() {
-  pinMode(redLight1, OUTPUT);
-  pinMode(yellowLight1, OUTPUT);
-  pinMode(greenLight1, OUTPUT);
+  pinMode(redLightA, OUTPUT);
+  pinMode(yellowLightA, OUTPUT);
+  pinMode(greenLightA, OUTPUT);
 
-  pinMode(redLight2, OUTPUT);
-  pinMode(yellowLight2, OUTPUT);
-  pinMode(greenLight2, OUTPUT);
+  pinMode(redLightB, OUTPUT);
+  pinMode(yellowLightB, OUTPUT);
+  pinMode(greenLightB, OUTPUT);
+
   lc.shutdown(0, false);
-  lc.setIntensity(0,8);
+  lc.setIntensity(0, 8);
   lc.clearDisplay(0);
 }
 
 void runRedLights() {
-    digitalWrite(greenLight1, HIGH);
-    digitalWrite(yellowLight1, LOW);
-    digitalWrite(redLight1, LOW);
-    digitalWrite(greenLight2, HIGH);
-    digitalWrite(yellowLight2, LOW);
-    digitalWrite(redLight2, LOW);
+  digitalWrite(greenLightA, HIGH);
+  digitalWrite(yellowLightA, LOW);
+  digitalWrite(redLightA, LOW);
+
+  digitalWrite(greenLightB, HIGH);
+  digitalWrite(yellowLightB, LOW);
+  digitalWrite(redLightB, LOW);
 }
 
 void runYellowLights() {
-    digitalWrite(greenLight1, LOW);
-    digitalWrite(yellowLight1, HIGH);
-    digitalWrite(redLight1, LOW);
-    digitalWrite(greenLight2, LOW);
-    digitalWrite(yellowLight2, HIGH);
-    digitalWrite(redLight2, LOW);
+  digitalWrite(greenLightA, LOW);
+  digitalWrite(yellowLightA, HIGH);
+  digitalWrite(redLightA, LOW);
+
+  digitalWrite(greenLightB, LOW);
+  digitalWrite(yellowLightB, HIGH);
+  digitalWrite(redLightB, LOW);
 }
 
 void runGreenLights() {
-    digitalWrite(greenLight1, LOW);
-    digitalWrite(yellowLight1, LOW);
-    digitalWrite(redLight1, HIGH);
-    digitalWrite(greenLight2, LOW);
-    digitalWrite(yellowLight2, LOW);
-    digitalWrite(redLight2, HIGH);
+  digitalWrite(greenLightA, LOW);
+  digitalWrite(yellowLightA, LOW);
+  digitalWrite(redLightA, HIGH);
+
+  digitalWrite(greenLightB, LOW);
+  digitalWrite(yellowLightB, LOW);
+  digitalWrite(redLightB, HIGH);
 }
 
 void runSmileyFace() {
   lc.clearDisplay(0);
   lc.setColumn(0, 2, B01100110);
   lc.setColumn(0, 5, B01000010);
-  lc.setColumn(0,6,B01111110);
+  lc.setColumn(0, 6, B01111110);
 }
 
 void runHLetter() {
   lc.clearDisplay(0);
   lc.setRow(0, 1, B11111111);
   lc.setRow(0, 2, B11111111);
-
   lc.setRow(0, 5, B11111111);
   lc.setRow(0, 6, B11111111);
-
   lc.setColumn(0, 3, B01111110);
   lc.setColumn(0, 4, B01111110);
   lc.setColumn(0, 5, B01111110);
@@ -88,21 +92,17 @@ void loop() {
   if (counter == 0) {
     runRedLights();
     runSmileyFace();
-  }
-
-  else if (counter == 1) {
+  } else if (counter == 1) {
     runYellowLights();
     runHLetter();
-  }
-
-  else if (counter == 2){
+  } else if (counter == 2) {
     runGreenLights();
     runILetter();
   }
 
   counter++;
 
-  // Resetting needs to be below counter++ or else the counter will increment to 1 before next loop 
+  // Reset counter after reaching the last state
   if (counter > 2) {
     counter = 0;
   }
